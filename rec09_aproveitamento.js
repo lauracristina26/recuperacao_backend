4534cce3fdacbe0142cf50076497b18da341e985
lauracristina26 <lauracristinalucas10@gmail.com>
2026-09-23 11:18:12 -0300

    Exercicio 09 - Recuperacao 23.09.26

--- rec09_aproveitamento.js
+++ rec09_aproveitamento.js
@@ -1,5 +1,27 @@
 const entrada = require('readline-sync');
 
 function calcularAproveitamento(util, total) {
-  
+  return (util / total) * 100;
 }
+
+function classificarAproveitamento(percentual) {
+  if (percentual >= 90) {
+    return "EXCENLENTE";
+  } else if (percentual >= 75 && percentual <=89.99) {
+    return "ADEQUADO";
+  } else {
+    return "REVISAR PROCESSO";
+  }
+}
+
+const qtdTotal = entrada.questionFloat("Digite a quantidade total: ");
+const qtdUtil = entrada.questionFloat ("Digite a quantidade util: ");
+
+const aproveitamento = calcularAproveitamento(util, total);
+const classificacao = classificarAproveitamento(percentual);
+
+console.log(`\n=== RELATORIO DE APROVEITAMENTO ===`);
+console.log(`Total: ${qtdTotal}`);
+console.log(`Quantidade Util: ${qtdUtil}`);
+console.log(`Aproveitamento: ${aproveitamento.toFixed(2)}%`);
+console.log(`Classificação: ${classificacao}`);
